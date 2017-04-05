2fd1d0dc3ac272b2ba8f6233b6682bb133f9cf01
DW2733 <dw2733@tc.columbia.edu>
2017-04-05 12:58:00 -0400

    master3

--- app/src/main/java/com/example/dadriaunna01/takehomeassignment09_dadriaunnaw/MainActivity.java
+++ app/src/main/java/com/example/dadriaunna01/takehomeassignment09_dadriaunnaw/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 import static android.transition.Fade.IN;
